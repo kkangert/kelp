58b65802c15db54272b1478e81bc0ba0b20592a9
kangert <kangert@qq.com>
2024-08-25 15:18:38 +0800

    新增 支持对List类型的内部数组访问

--- src/main/java/top/kangert/elp/expression/ArrayAccess.java
+++ src/main/java/top/kangert/elp/expression/ArrayAccess.java
@@ -1,5 +1,7 @@
 package top.kangert.elp.expression;
 
+import java.util.List;
+
 public class ArrayAccess extends Expression {
     private final String identifier;
     private final Expression indexExpression;
@@ -12,13 +14,34 @@ public class ArrayAccess extends Expression {
     @Override
     public Object evaluate(Environment env) throws Exception {
         Object array = env.getVariable(identifier);
-        if (!(array instanceof Object[])) {
+
+        if (array instanceof Object[]) {
+            // 数据强转
+            Object[] list = (Object[]) array;
+            // 检查下标
+            int idx = checkIndex(env, list.length);
+            // 获取元素
+            return list[idx];
+        } else if (array instanceof List) {
+            // 数据强转
+            List<Object> list = (List<Object>) array;
+            // 检查下标
+            int idx = checkIndex(env, list.size());
+            // 获取元素
+            return list.get(idx);
+        } else {
             throw new Exception("Expected an array but got " + array.getClass().getSimpleName());
         }
+    }
 
-        // 数据强转
-        Object[] list = (Object[]) array;
-
+    /**
+     * 检查下标合法性
+     * @param env 上下文环境
+     * @param arrSize 数组大小
+     * @return 合法的下标
+     * @throws Exception
+     */
+    private int checkIndex(Environment env, int arrSize) throws Exception {
         // 支持arr[exp]形式的访问
         Object index = indexExpression.evaluate(env);
         if (!(index instanceof Integer)) {
@@ -26,11 +49,10 @@ public class ArrayAccess extends Expression {
         }
 
         int idx = (Integer) index;
-        if (idx < 0 || idx >= list.length) {
+        if (idx < 0 || idx >= arrSize) {
             throw new Exception("Index out of bounds: " + idx);
         }
-
-        return list[idx];
+        return idx;
     }
 
     @Override
